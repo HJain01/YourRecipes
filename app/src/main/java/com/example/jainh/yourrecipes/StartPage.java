4caffd4c32c30a1ce7229ac81c0ed2ee4de00957
Harsh Jain <jainh9999@gmail.com>
2017-11-10 11:15:32 -0500

    Redoing the start page

--- app/src/main/java/com/example/jainh/yourrecipes/StartPage.java
+++ app/src/main/java/com/example/jainh/yourrecipes/StartPage.java
@@ -1,7 +1,10 @@
 package com.example.jainh.yourrecipes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        Button button = (Button) findViewById(R.id.startButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStartPress();
+            }
+        });
+    }
+
+    public void onStartPress() {
+        Intent intent = new Intent(this, PreferencesPage.class);
+        startActivity(intent);
     }
 }
