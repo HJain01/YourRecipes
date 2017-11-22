07eee2d430a05592e64503115c73d64ff9d04fc8
Harsh Jain <jainh9999@gmail.com>
2017-11-22 17:26:47 -0500

    Added a preferences page

---
+++ app/src/main/java/com/example/jainh/yourrecipes/PreferencesPage.java
@@ -0,0 +1,13 @@
+package com.example.jainh.yourrecipes;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class PreferencesPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences_page);
+    }
+}
